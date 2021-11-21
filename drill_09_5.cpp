ac0b97fd1054c2e4ed22559994f5271a445f2b2f
justoneguy01 <90860513+justoneguy01@users.noreply.github.com>
2021-11-21 17:49:51 +0100

    Update drill_09_5.cpp

--- drill_09_5.cpp
+++ drill_09_5.cpp
@@ -6,19 +6,19 @@ class Year{
 public:
     class Invalid{};
     Year(int x): y{x} {if (x < min || x > max ) throw Invalid{};}
-    int year() {return y;} //get függvény
+    int year() {return y;} 
     int increment() {y++;}
 private:
     int y;
 
 };
 
-Year operator++(Year& year){ //lépteti az évet
+Year operator++(Year& year){ 
     year.increment();
     return year;
 }
 
-ostream& operator<<(ostream& os, Year year){ //kiíratás
+ostream& operator<<(ostream& os, Year year){ 
     return os << year.year();
 }
 
@@ -42,39 +42,38 @@ enum class Month {
     jan, feb, mar, apr, may, jun, jul, aug, sept, oct, nov, dec
 };
 
-Month operator++(Month& m){ //Monthal térünk vissza
+Month operator++(Month& m){ 
     m = (m==Month::dec) ? Month::jan : Month(int(m)+1);
 	return m;
 }
 
-//kiiratás:
+
 ostream& operator<<(ostream& os, Month m){
     return os << months[int(m)];
 }
 
 class Date{
-private: //ezeket kívülről nem tudjuk majd elérni
+private: 
     Year year;
     Month month;
-    int day; //3 tag a structnak
-public:   //ezeket igen
+    int day; 
+public:   
     class Invalid{};
-    //default constructor létrehozása vektor miatt
     Date(): year(Year{2001}), month(Month::jan), day(1) {}
-    Date(Year y, Month m, int d): year(y), month(m), day(d){ if(!is_valid()) throw Invalid{}; } //Konstruktor: segítségével megtudjuk határozni hogyan jöjjön létre az objektum
-    void add_day(int n); //nap hozzáadása
+    Date(Year y, Month m, int d): year(y), month(m), day(d){ if(!is_valid()) throw Invalid{}; } 
+    void add_day(int n); 
 
-    //Dátum ellenörzése, hogy érvényes-e
+
     bool is_valid();
 
-    //getter függvény: eltudjuk kérni a tagnak az értékét -> kiíratás
+  
     Year get_year() const {return year;}
     Month get_month() const  {return month;}
     int get_day() const  {return day;}
 
     
 
-    //setter függvények: betudjuk állítani az értéket
+
     void set_year(Year y){
 
         year = y;
@@ -102,7 +101,7 @@ bool Date::is_valid(){
 }
 
 void Date::add_day(int n)
-{ //Hozzáad valamennyi napot egy paraméterül adott naphoz.
+{ 
 
     day += n;
     while (day > 31)
@@ -123,7 +122,7 @@ int main()
     try
     {
 
-        Date today {Year{2021}, Month::dec, 23}; //Példány készítése
+        Date today {Year{2021}, Month::dec, 23};
         std::cout << "Date: " << today;
         
         today.add_day(1); 
