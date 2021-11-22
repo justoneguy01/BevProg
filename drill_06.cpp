7f49cfb4e78aed247dac1cf17f757039cb220a6a
justoneguy01 <90860513+justoneguy01@users.noreply.github.com>
2021-11-22 14:15:14 +0100

    Update drill_06.cpp

--- drill_06.cpp
+++ drill_06.cpp
@@ -1,40 +1,25 @@
-//
-// This is example code from Chapter 6.7 "Trying the second version" of
-// "Software - Principles and Practice using C++" by Bjarne Stroustrup
-//
-
-/*
-    This file is known as calculator02buggy.cpp
-    I have inserted 5 errors that should cause this not to compile
-    I have inserted 3 logic errors that should cause the program to give wrong results
-    First try to find an remove the bugs without looking in the book.
-    If that gets tedious, compare the code to that in the book (or posted source code)
-    Happy hunting!
-*/
-
-//PASS (Hibás)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-#include "std_lib_facilities.h"
+#include "../../std_lib_facilities.h"
 
 //------------------------------------------------------------------------------
 
-class Token{          // c betű hiánya
+class Token { //Első compile hiba, lemaradt a c betű a class-ból
 public:
     char kind;        // what kind of token
     double value;     // for numbers: a value 
     Token(char ch)    // make a Token from a char
-        :kind(ch), value(0) { }
+        :kind(ch), value(0) { }    
     Token(char ch, double val)     // make a Token from a char and a double
         :kind(ch), value(val) { }
 };
 
 //------------------------------------------------------------------------------
+
 class Token_stream {
-public:
+public: 
     Token_stream();   // make a Token_stream that reads from cin
     Token get();      // get a Token (get() is defined elsewhere)
     void putback(Token t);    // put a Token back
-private:
+private:   
     bool full;        // is there a Token in the buffer?
     Token buffer;     // here is where we keep a Token put back using putback()
 };
@@ -43,7 +28,7 @@ private:
 
 // The constructor just sets full to indicate that the buffer is empty:
 Token_stream::Token_stream()
-    :full(false), buffer(0)    // no Token in buffer
+:full(false), buffer(0)    // no Token in buffer
 {
 }
 
@@ -59,13 +44,13 @@ void Token_stream::putback(Token t)
 
 //------------------------------------------------------------------------------
 
-Token Token_stream::get()
+Token Token_stream::get() //Második compile hiba, a Token Stream classból akarjuk elérni a Get-et
 {
     if (full) {       // do we already have a Token ready?
         // remove token from buffer
-        full = false;
+        full=false;
         return buffer;
-    }
+    } 
 
     char ch;
     cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
@@ -73,21 +58,23 @@ Token Token_stream::get()
     switch (ch) {
     case '=':    // for "print"
     case 'x':    // for "quit"
-    case '(': case ')': case '+': case '-': case '*': case '/':
+    case '(': case ')': case '+': case '-': case '*': case '/': 
         return Token(ch);        // let each character represent itself
     case '.':
     case '0': case '1': case '2': case '3': case '4':
-    case '5': case '6': case '7': case '8':case '9':   //kimaradt a 8 as
-    {
-        cin.putback(ch);         // put digit back into the input stream
-        double val;
-        cin >> val;              // read a floating-point number
-        return Token('k', val);   // let 'k' represent "a number"
-    }
+    case '5': case '6': case '7': case '8': case '9':  
+    //Első logikai hiba, ha a 8-ast a számok típusaként használjuk, a 8-as számmal nem tudunk számolni, tehát át kell írni itt, és a többi helyen ahol hivatkoztunk rá
+        {    
+            cin.putback(ch);         // put digit back into the input stream
+            double val;
+            cin >> val;              // read a floating-point number
+            return Token('n',val);   // let 'n' represent "a number"
+        }
     default:
         error("Bad token");
+    
     }
-    return -1;
+    return -1;  // 4. compile hiba. Konvenció miatt vissza kell térni valamivel, még ha nem is jut el soha ide.
 }
 
 //------------------------------------------------------------------------------
@@ -106,77 +93,87 @@ double primary()
     Token t = ts.get();
     switch (t.kind) {
     case '(':    // handle '(' expression ')'
-    {
-        double d = expression();
-        t = ts.get();
-        if (t.kind != ')') error(" ')' expected"); //Szintaktikai hiba!!!!!!
+        {    
+            double d = expression();
+            t = ts.get();
+            if (t.kind != ')') error("')'expected"); //Harmadik compile error, lemaradt egy "
             return d;
-    }
-    case 'k':            // we use '8' to represent a number
+        }
+    case 'n':            // we use 'n' to represent a number
         return t.value;  // return the number's value
     default:
         error("primary expected");
+    
     }
-    return 1;
+    return -1; //5. compile hiba. Itt is konvenció miatt vissza kell térni valamivel
 }
+
 //------------------------------------------------------------------------------
+
 // deal with *, /, and %
 double term()
 {
     double left = primary();
     Token t = ts.get();        // get the next token from token stream
-    while (true) {
+
+    while(true) {
         switch (t.kind) {
         case '*':
             left *= primary();
             t = ts.get();
-        case '/':
-        {
-            double d = primary();
-            if (d == 0) error("divide by zero");
-            left /= d;
-            t = ts.get();
             break;
-        }
-        default:
+        case '/':
+            {    
+                double d = primary();
+                if (d == 0) error("divide by zero");
+                left /= d; 
+                t = ts.get();
+                break;
+            }
+        default: 
             ts.putback(t);     // put t back into the token stream
             return left;
         }
     }
 }
+
 //------------------------------------------------------------------------------
+
 // deal with + and -
 double expression()
 {
     double left = term();      // read and evaluate a Term
     Token t = ts.get();        // get the next token from token stream
-    while (true) {
-        switch (t.kind) {
+
+    while(true) {    
+        switch(t.kind) {
         case '+':
             left += term();    // evaluate Term and add
             t = ts.get();
             break;
         case '-':
-            left -= term();    // evaluate Term and subtract
+            left -= term();    // evaluate Term and subtract  //Harmadik logikai hiba, kivonásnál kivonni kell, nem összeadni
             t = ts.get();
             break;
-        default:
+        default: 
             ts.putback(t);     // put t back into the token stream
             return left;       // finally: no more + or -: return the answer
         }
     }
 }
+
 //------------------------------------------------------------------------------
-double val = 0;
+double val=0;
 int main()
+{
 try
 {
-    cout << "Welcome to our simple calculator." << endl;
-    cout << "Please enter expressions using fl oating-point numbers." << endl;
-    cout << "Operators: (,),+,-,*,/" << endl;
-    cout << "Print: '=' ,Exit : 'x' " << endl;
+    cout<<"Welcome to our simple calculator. Please enter expressions using floating-point numbers \n";
+    cout<<"The operators you can use are:(,),+,-,*,/ \n";
+    cout<<"If you want to print now, press ; , and if you want to exit print q \n";
     while (cin) {
         Token t = ts.get();
+
         if (t.kind == 'x') break; // 'q' for quit
         if (t.kind == '=')        // ';' for "print now"
             cout << "=" << val << '\n';
@@ -184,16 +181,19 @@ try
             ts.putback(t);
         val = expression();
     }
-    keep_window_open();
+	keep_window_open();
 }
-catch (exception& e) {
-    cerr << "error: " << e.what() << '\n';
-    keep_window_open();
+catch (exception& e)
+{
+    cerr << "error: " << e.what() << '\n'; 
+	keep_window_open();
     return 1;
 }
-catch (...) {
-    cerr << "Oops: unknown exception!\n";
-    keep_window_open();
+catch (...)
+{
+    cerr << "Oops: unknown exception!\n"; 
+	keep_window_open();
     return 2;
+}
 }
 //------------------------------------------------------------------------------
