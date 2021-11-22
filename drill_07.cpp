f7aec6a7b70bedce3bb384f4f9427029a19a9477
justoneguy01 <90860513+justoneguy01@users.noreply.github.com>
2021-11-22 14:16:29 +0100

    Update drill_07.cpp

--- drill_07.cpp
+++ drill_07.cpp
@@ -1,213 +1,249 @@
-
-/*
-	calculator08buggy.cpp
-	Helpful comments removed.
-	We have inserted 3 bugs that the compiler will catch and 3 that it won't.
-*/
-//PASS (Hibás)!!!!!!!!!!!!!!!!!!!!!!!!
-
-#include "std_lib_facilities.h"
+#include "../../std_lib_facilities.h"
 
 struct Token {
-	char kind;
+	char kind; //Létrehozzuk a token struktúrát, 3 változóval, a típussal, értékkel, és névvel
 	double value;
 	string name;
-	Token(char ch) :kind(ch), value(0) { }
-	Token(char ch, double val) :kind(ch), value(val) { }
-	Token(char ch, string s): kind(ch), name(s) {} 
+	Token(char ch) :kind(ch), value(0) { } //Token a karaktereknek mint: ;,(, stb.
+	Token(char ch, double val) :kind(ch), value(val) { } //Token a számoknak
+	Token(char ch, string s): kind(ch), name(s) {}  //Token a váltizók deklarálásához
+	//Első compile hiba, enélkül a sor nélkül nem tudunk string-es tokeneket feldolgozni
+
 };
-class Token_stream {
+
+class Token_stream 
+{
 	bool full;
 	Token buffer;
 public:
 	Token_stream() :full(0), buffer(0) { }
-
 	Token get();
-	void unget(Token t) { buffer = t; full = true; }
-
+	void unget(Token t) { buffer=t; full=true; } 
 	void ignore(char);
 };
+//Token stream osztály függvényeinek definiálása
 
+
+const char set ='S';
 const char let = 'L';
-const char quit = 'Q';
+const char quit = 'q'; 
 const char print = ';';
 const char number = '8';
 const char name = 'a';
+const char sqr='r';
+const char power='P';
+const string close = "exit";
+const string declarkey = "#";
+const string redeclar ="set";
+const string squareroot="sqrt";
+const string powerof="pow";
+//Konstantsok tokenek kezeléséhez
 
-Token Token_stream::get()
+Token Token_stream::get()  //Egy karakter bekérése az input streamből, majd abból Token készítése
 {
-	if (full) { full = false; return buffer; }
+	if (full) { full=false; return buffer; }
 	char ch;
 	cin >> ch;
-	switch (ch) 
-	{
-		case '(':
-		case ')':
-		case '+':
-		case '-':
-		case '*':
-		case '/':
-		case '%':
-		case ';':
-		case '=':
-		case 'quit':
-		case 'print':
-			return Token(ch);
-		case '.':
-		case '0':
-		case '1':
-		case '2':
-		case '3':
-		case '4':
-		case '5':
-		case '6':
-		case '7':
-		case '8':
-		case '9':
-	{	
-		cin.unget();
+	switch (ch) {
+	case '(':
+	case ')':
+	case '+':
+	case '-':
+	case '*':
+	case '/':
+	case '%':
+	case print:
+	case quit: //Első logikai hiba, kell a 'q' a kilépéshez
+	case '=':
+	case ',':
+		return Token(ch); //Ez az első típusó Token
+	case '.':
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+	{	cin.unget(); // Mivel egy szám karaktert találtunk, azt vissza kell raknunk az input streambe, hogy ki tudjuk kérni a szám egészét
 		double val;
-		cin >> val;
-		return Token(number, val);
+		cin >> val; //A szám egészének bekérése val változóba
+		return Token(number,val); //Szám típusó Token visszaadása
 	}
 	default:
-		if (isalpha(ch)) {
+		if ((isalpha(ch)) || (ch=='#'))  //Változók kezelése
+		{
 			string s;
 			s += ch;
-			while (cin.get(ch) && (isalpha(ch) || isdigit(ch))) s = ch;
+			while(cin.get(ch) && (isalpha(ch) || isdigit(ch))) s+=ch; // Második logikai hiba, alapból mindig felülírja az s karaktert, hozzá kell fűzni.
 			cin.unget();
-			if (s == "let") return Token(let);
-			if (s == "quit") return Token(name);
-			return Token(name, s);
+			if (s == declarkey) return Token(let);
+			if (s == redeclar) return Token(set);
+			if (s == squareroot) return Token(sqr);
+			if (s == powerof) return Token(power);
+			if (s == close) return Token(quit);
+			return Token(name,s);
+			//Különféle föggvényekhez tartozó kulcsszólhoz Token rendelése
 		}
+		cin.putback(';');
 		error("Bad token");
+		
 	}
+	return 0; //Második hiba, bár enélkül is lefordul a program, vissza kell térnünk az error után is valamivel.
 }
 
-void Token_stream::ignore(char c)
+void Token_stream::ignore(char c) //Error kezelés utáni további számolás lehetővé tétele
 {
-	if (full && c == buffer.kind) {
+	if (full && c==buffer.kind) {
 		full = false;
 		return;
 	}
 	full = false;
 
 	char ch;
-	while (cin >> ch)
-		if (ch == c) return;
+	while (cin>>ch)
+		if (ch==c) return;  //Amíg eljut a c karakterig folyamatosan kéri be az input streamből a változókat
 }
 
-struct Variable {
+struct Variable { //Változókhoz szükséges struktúra elkészítése, melynek van neve, és értéke.
 	string name;
 	double value;
-	Variable(string n, double v) :name(n), value(v) { }
+	Variable(string n, double v) :name(n), value(v) { } //Példa változó elkészítése
 };
 
-vector<Variable> names;
+vector<Variable>names;	//A változók eltárolására egy vektor deifiniálása
 
 double get_value(string s)
 {
-	for (int i = 0; i < names.size(); ++i)
-		if (names[i].name == s) return names[i].value;
-	error("get: undefined name ", s);
+	for (auto& v : names ) //Második logikai hiba, a változók megadásakor
+		if (v.name == s) return v.value;
+	cin.putback(';');
+	error("get: undefined name ",s);
+	
+	return 0; //Harmadik hiba, bár enélkül is lefordul a program, vissza kell térnünk az error után is valamivel.
+	//Ha kaptunk egy változót, megnézzük az benne van-e a változó táblában, ha igen visszaadjuk értéket, ha nem errort adunk.
 }
 
-void set_value(string s, double d)
+void set_value(string s, double d) //Változók újradeklarása
 {
-	for (int i = 0; i <= names.size(); ++i)
+	for (int i = 0; i<=names.size(); ++i)
 		if (names[i].name == s) {
 			names[i].value = d;
 			return;
 		}
-	error("set: undefined name ", s);
+	cin.putback(';');
+	error("set: undefined name ",s);
+	
 }
 
-bool is_declared(string s)
+bool is_declared(string s)  //Függvény arra, hogy ne tudjunk kétszer deklarálni egy változót.
 {
-	for (int i = 0; i < names.size(); ++i)
+	for (int i = 0; i<names.size(); ++i)
 		if (names[i].name == s) return true;
 	return false;
 }
 
-Token_stream ts;
+Token_stream ts; //Egy deklaráció a Token_stream-ből, hogy tudjuk használni.
 
-double expression();
+double expression(); //Egy deklaráció az expression-ből, hogy tudjuk használni.
 
 double primary()
 {
 	Token t = ts.get();
-	switch (t.kind) {
-	case '(':
-	{	double d = expression();
-	t = ts.get();
-	if (t.kind != ')') error("'(' expected");
+	switch (t.kind) 
+	{
+		case '(':
+		{	
+			double d = expression();
+			t = ts.get();
+			if (t.kind != ')') error("')' expected");
+			return d; // Harmadik logika hiba, vissza kell térni a d-vel, hogy működjön a zárójel.
+		}
+		case number:
+			return t.value;
+		case '-':
+			return - primary(); //Negatív számok kezelése
+		case name:
+			return get_value(t.name); //Változó értékének bekérése
+		default:
+			cin.putback(';');
+			error("primary expected");
 	}
-	case '-':
-		return -primary();
-	case number:
-		return t.value;
-	case name:
-		return get_value(t.name);
-	default:
-		error("primary expected");
-	}
+	return -1; //Negyedik hiba, bár enélkül is lefordul a program, vissza kell térnünk az error után is valamivel.
 }
 
-double term()
+double term()  //Szorzás és osztás kezelése, errorkezelés
 {
 	double left = primary();
-	while (true) {
-		Token t = ts.get();
-		switch (t.kind) {
-		case '*':
-			left *= primary();
-			break;
-		case '/':
-		{	double d = primary();
-		if (d == 0) error("divide by zero");
-		left /= d;
-		break;
-		}
-		default:
-			ts.unget(t);
-			return left;
+	Token t = ts.get();  
+	while(true) {
+		
+		switch(t.kind) {
+			case '*':
+				left *= primary();
+				t=ts.get();
+				break;
+			case '/':
+			{	double d = primary();
+				if (d == 0) error("divide by zero");
+				left /= d;
+				t=ts.get();
+				break;
+			}
+			case '%':
+			{
+				double d = primary();
+				if (d == 0) error("%: divide by zero");
+				left = fmod (left, d);
+				t = ts.get();
+				break;
+			}
+			default:
+				ts.unget(t);
+				return left;
 		}
 	}
 }
 
-double expression()
+double expression() //Összeadás és kivonás kezelése
 {
 	double left = term();
-	while (true) {
-		Token t = ts.get();
-		switch (t.kind) {
-		case '+':
-			left += term();
-			break;
-		case '-':
-			left -= term();
-			break;
-		default:
-			ts.unget(t);
-			return left;
+	Token t = ts.get(); //
+	while(true) {
+		switch(t.kind) 
+		{
+			case '+':
+				left += term();
+				t=ts.get();
+				break;
+			case '-':
+				left -= term();
+				t=ts.get();
+				break;
+			default:
+				ts.unget(t);
+				return left;
 		}
 	}
 }
 
-double declaration()
+double declaration()  //Változó deklarálása
 {
 	Token t = ts.get();
-	if (t.kind != 'a') error("name expected in declaration");
+	if (t.kind != 'a') error ("name expected in declaration");
 	string name = t.name;
 	if (is_declared(name)) error(name, " declared twice");
 	Token t2 = ts.get();
-	if (t2.kind != '=') error("= missing in declaration of ", name);
+	if (t2.kind != '=') error("= missing in declaration of " ,name);
 	double d = expression();
-	names.push_back(Variable(name, d));
+	names.push_back(Variable(name,d));
 	return d;
 }
 
-double powerx() 
+double powerx() //Hatványozás megvalósítása
 {
 	Token t1=ts.get();
 	if (t1.kind!='(') error("'(' expected at the beginning of pow ");
@@ -228,7 +264,7 @@ double powerx()
 	return result;
 }
 
-double fsqrt() 
+double fsqrt() //Gyökvönás megvalósítása
 {
 	double d=expression();
 	if (d <0) error("Can't take the squareroot of a negative number in the domain of real numbers");
@@ -237,21 +273,40 @@ double fsqrt()
 		return sqrt(d);
 		}
 	return 0;
-}
+}	
 
-double statement()
+
+double statement() //Eldöntjük mit kaptunk, egy számot, változó deklarálást, gyökvonást vagy hatványozást
 {
 	Token t = ts.get();
-	switch (t.kind) {
+	switch(t.kind) {
 	case let:
 		return declaration();
+	case sqr:
+		return fsqrt();
+	case power:
+		return powerx();
+	case set: //Nemtudom ez vehető-e harmadik logikai hibának, de meg volt írva a set_value függvény, és nem volt használva, ezért megcsináltam, hogy működjön.
+		{
+		Token t1=ts.get();
+		if (is_declared(t1.name))
+		{
+			Token t2 = ts.get();
+			if (t2.kind != '=') error("= missing in redeclaration of " ,name);
+			double d=expression();
+			set_value(t1.name, d);
+		}
+		
+		else (error("Can't redeclar a variable if it wasn't declared in the first place."));
+
+		}
 	default:
 		ts.unget(t);
 		return expression();
 	}
 }
 
-void clean_up_mess()
+void clean_up_mess()  //Error utáni számolás lehetővé tétele
 {
 	ts.ignore(print);
 }
@@ -261,35 +316,35 @@ const string result = "= ";
 
 void calculate()
 {
-	while (true) try {
+	while(true) try {
 		cout << prompt;
 		Token t = ts.get();
-		while (t.kind == print) t = ts.get();
+		while (t.kind == print) t=ts.get();  //Megnézzük a kulcskaraktereket, mint q és ;
 		if (t.kind == quit) return;
 		ts.unget(t);
 		cout << result << statement() << endl;
 	}
-	catch (runtime_error& e) {
+	catch(runtime_error& e) {
 		cerr << e.what() << endl;
 		clean_up_mess();
 	}
 }
 
 int main()
-
-try {
-	calculate();
-	return 0;
-}
-catch (exception& e) {
-	cerr << "exception: " << e.what() << endl;
-	char c;
-	while (cin >> c && c != ';');
-	return 1;
-}
-catch (...) {
-	cerr << "exception\n";
-	char c;
-	while (cin >> c && c != ';');
-	return 2;
-}
+	try {
+		names.push_back(Variable("k",1000)); //Változó előre definiálása
+		calculate();
+		return 0;
+	}
+	catch (exception& e) {
+		cerr << "exception: " << e.what() << endl;
+		char c;
+		while (cin >>c&& c!=';') ;
+		return 1;
+	}
+	catch (...) {
+		cerr << "exception\n";
+		char c;
+		while (cin>>c && c!=';');
+		return 2;
+	}
